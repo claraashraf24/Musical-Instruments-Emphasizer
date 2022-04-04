2f9e8f177427eeb1c894f9d2c55e2b0f46d232ee
Clara Ashraf <97061363+claraashraf24@users.noreply.github.com>
2022-04-04 02:50:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from numpy.fft import fft, ifft
 from PyQt5 import QtWidgets, uic, QtCore
-from PyQt5.QtWidgets import QSlider
+from PyQt5.QtWidgets import QSlider, QLabel
 from PyQt5.QtCore import QUrl
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 import os
@@ -24,36 +24,49 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.signal_plot)
         self.pause_button.clicked.connect(self.pause)
         self.play_button.clicked.connect(self.replay)
-        self.label_6.mousePressEvent  = self.piano
-        self.label_4.mousePressEvent = self.drums
-        self.label_5.mousePressEvent = self.xylophone
+        self.piano_label.mousePressEvent  = self.piano
+        self.drums_label.mousePressEvent = self.drums
+        self.xylo_label.mousePressEvent = self.xylophone
+        #define slider widget
+        self.volume_slider = self.findChild(QSlider, "volume_slider")
+        self.volume_number = self.findChild(QLabel, "volume_number")
+        #move volume slider
+        self.volume_number.setAlignment(QtCore.Qt.AlignCenter)
+        self.volume_slider.valueChanged.connect(self.move_slider)
+        self.volume_slider.setMaximum(100)
+
+    def move_slider(self, value):
+        self.volume_number.setText(str(value))
+
+
 
     def xylophone(self, event):
          x= event.pos().x()
          y= event.pos().y()
 
-         if x/self.label_5.size().width() > 0.05 and x/self.label_5.size().width() < 0.14 and y/self.label_5.size().width() > 0.15 and y/self.label_5.size().width() < 0.30 :
+         if x/self.xylo_label.size().width() > 0.05 and x/self.xylo_label.size().width() < 0.14 and y/self.xylo_label.size().width() > 0.15 and y/self.xylo_label.size().width() < 0.30 :
             print("Bar 1") 
+            
 
-         if x/self.label_5.size().width() > 0.16 and x/self.label_5.size().width() < 0.25 and y/self.label_5.size().width() > 0.13 and y/self.label_5.size().width() < 0.32 :
+         if x/self.xylo_label.size().width() > 0.16 and x/self.xylo_label.size().width() < 0.25 and y/self.xylo_label.size().width() > 0.13 and y/self.xylo_label.size().width() < 0.32 :
             print("Bar 2")
 
-         if x/self.label_5.size().width() > 0.28 and x/self.label_5.size().width() < 0.37 and y/self.label_5.size().width() > 0.12 and y/self.label_5.size().width() < 0.33 :
+         if x/self.xylo_label.size().width() > 0.28 and x/self.xylo_label.size().width() < 0.37 and y/self.xylo_label.size().width() > 0.12 and y/self.xylo_label.size().width() < 0.33 :
             print("Bar 3")
 
-         if x/self.label_5.size().width() > 0.40 and x/self.label_5.size().width() < 0.49 and y/self.label_5.size().width() > 0.1 and y/self.label_5.size().width() < 0.33 :
+         if x/self.xylo_label.size().width() > 0.40 and x/self.xylo_label.size().width() < 0.49 and y/self.xylo_label.size().width() > 0.1 and y/self.xylo_label.size().width() < 0.33 :
             print("Bar 4")
 
-         if x/self.label_5.size().width() > 0.51 and x/self.label_5.size().width() < 0.60 and y/self.label_5.size().width() > 0.08 and y/self.label_5.size().width() < 0.36 :
+         if x/self.xylo_label.size().width() > 0.51 and x/self.xylo_label.size().width() < 0.60 and y/self.xylo_label.size().width() > 0.08 and y/self.xylo_label.size().width() < 0.36 :
             print("Bar 5")
 
-         if x/self.label_5.size().width() > 0.63 and x/self.label_5.size().width() < 0.72 and y/self.label_5.size().width() > 0.05 and y/self.label_5.size().width() < 0.38 :
+         if x/self.xylo_label.size().width() > 0.63 and x/self.xylo_label.size().width() < 0.72 and y/self.xylo_label.size().width() > 0.05 and y/self.xylo_label.size().width() < 0.38 :
             print("Bar 6")
 
-         if x/self.label_5.size().width() > 0.75 and x/self.label_5.size().width() < 0.84 and y/self.label_5.size().width() > 0.03 and y/self.label_5.size().width() < 0.4 :
+         if x/self.xylo_label.size().width() > 0.75 and x/self.xylo_label.size().width() < 0.84 and y/self.xylo_label.size().width() > 0.03 and y/self.xylo_label.size().width() < 0.4 :
             print("Bar 7")
         
-         if x/self.label_5.size().width() > 0.86 and x/self.label_5.size().width() < 0.96 and y/self.label_5.size().width() > 0.2 and y/self.label_5.size().width() < 0.42 :
+         if x/self.xylo_label.size().width() > 0.86 and x/self.xylo_label.size().width() < 0.96 and y/self.xylo_label.size().width() > 0.2 and y/self.xylo_label.size().width() < 0.42 :
             print("Bar 8")
 
 
@@ -64,25 +77,25 @@ class MainWindow(QtWidgets.QMainWindow):
     def drums(self, event):
         x= event.pos().x()
         y= event.pos().y()
-        if x/self.label_4.size().width() > 0.027 and x/self.label_4.size().width() < 0.26 and y/self.label_4.size().width() > 0.009 and y/self.label_4.size().width() < 0.07 :
+        if x/self.drums_label.size().width() > 0.027 and x/self.drums_label.size().width() < 0.26 and y/self.drums_label.size().width() > 0.009 and y/self.drums_label.size().width() < 0.07 :
             print("Ride Cymbal") 
 
-        if x/self.label_4.size().width() > 0.733 and x/self.label_4.size().width() < 1.00 and y/self.label_4.size().width() > 0.03 and y/self.label_4.size().width() < 0.10 :
+        if x/self.drums_label.size().width() > 0.733 and x/self.drums_label.size().width() < 1.00 and y/self.drums_label.size().width() > 0.03 and y/self.drums_label.size().width() < 0.10 :
             print("Hi-Hat Cymbal") 
 
-        if x/self.label_4.size().width() > 0.53 and x/self.label_4.size().width() < 0.69 and y/self.label_4.size().width() > 0.10 and y/self.label_4.size().width() < 0.25 :
+        if x/self.drums_label.size().width() > 0.53 and x/self.drums_label.size().width() < 0.69 and y/self.drums_label.size().width() > 0.10 and y/self.drums_label.size().width() < 0.25 :
             print("Medium Tom") 
 
-        if x/self.label_4.size().width() > 0.21 and x/self.label_4.size().width() < 0.50 and y/self.label_4.size().width() > 0.05 and y/self.label_4.size().width() < 0.20 :
+        if x/self.drums_label.size().width() > 0.21 and x/self.drums_label.size().width() < 0.50 and y/self.drums_label.size().width() > 0.05 and y/self.drums_label.size().width() < 0.20 :
             print("High Tom")
 
-        if x/self.label_4.size().width() > 0.05 and x/self.label_4.size().width() < 0.24 and y/self.label_4.size().width() > 0.15 and y/self.label_4.size().width() < 0.50 :
+        if x/self.drums_label.size().width() > 0.05 and x/self.drums_label.size().width() < 0.24 and y/self.drums_label.size().width() > 0.15 and y/self.drums_label.size().width() < 0.50 :
             print("Stand Tom") 
 
-        if x/self.label_4.size().width() > 0.41 and x/self.label_4.size().width() < 0.72 and y/self.label_4.size().width() > 0.30 and y/self.label_4.size().width() < 1.0 :
+        if x/self.drums_label.size().width() > 0.41 and x/self.drums_label.size().width() < 0.72 and y/self.drums_label.size().width() > 0.30 and y/self.drums_label.size().width() < 1.0 :
             print("Bass Drum") 
 
-        if x/self.label_4.size().width() > 0.76 and x/self.label_4.size().width() < 0.84 and y/self.label_4.size().width() > 0.20 and y/self.label_4.size().width() < 0.63 :
+        if x/self.drums_label.size().width() > 0.76 and x/self.drums_label.size().width() < 0.84 and y/self.drums_label.size().width() > 0.20 and y/self.drums_label.size().width() < 0.63 :
             print("Snare Drum") 
        
 
@@ -94,54 +107,54 @@ class MainWindow(QtWidgets.QMainWindow):
         x = event.pos().x()
         y = event.pos().y() 
 
-        if x/self.label_6.size().width() > 0.15 and x/self.label_6.size().width() < 0.20  and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.15 and x/self.piano_label.size().width() < 0.20  and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 1")            
-        if x/self.label_6.size().width() > 0.20 and x/self.label_6.size().width() < 0.25 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.20 and x/self.piano_label.size().width() < 0.25 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 2")
-        if x/self.label_6.size().width() > 0.25 and x/self.label_6.size().width() < 0.30 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.25 and x/self.piano_label.size().width() < 0.30 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 3")
-        if x/self.label_6.size().width() > 0.30 and x/self.label_6.size().width() < 0.35 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.30 and x/self.piano_label.size().width() < 0.35 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 4")
-        if x/self.label_6.size().width() > 0.35 and x/self.label_6.size().width() < 0.40 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.35 and x/self.piano_label.size().width() < 0.40 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 5")
-        if x/self.label_6.size().width() > 0.40 and x/self.label_6.size().width() < 0.45 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.40 and x/self.piano_label.size().width() < 0.45 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 6")
 
-        if x/self.label_6.size().width() > 0.45 and x/self.label_6.size().width() < 0.50 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.45 and x/self.piano_label.size().width() < 0.50 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 7")
-        if x/self.label_6.size().width() > 0.50 and x/self.label_6.size().width() < 0.55 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.50 and x/self.piano_label.size().width() < 0.55 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 8")
-        if x/self.label_6.size().width() > 0.55 and x/self.label_6.size().width() < 0.60 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.55 and x/self.piano_label.size().width() < 0.60 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 9")
-        if x/self.label_6.size().width() > 0.60 and x/self.label_6.size().width() < 0.65 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.60 and x/self.piano_label.size().width() < 0.65 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 10")
-        if x/self.label_6.size().width() > 0.65 and x/self.label_6.size().width() < 0.70 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.65 and x/self.piano_label.size().width() < 0.70 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 11")
-        if x/self.label_6.size().width() > 0.70 and x/self.label_6.size().width() < 0.75 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.70 and x/self.piano_label.size().width() < 0.75 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 12")
-        if x/self.label_6.size().width() > 0.75 and x/self.label_6.size().width() < 0.80 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.75 and x/self.piano_label.size().width() < 0.80 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 13")
-        if x/self.label_6.size().width() > 0.80 and x/self.label_6.size().width() < 0.85 and y/self.label_6.size().width() > 0.30 and y/self.label_6.size().width() < 0.40 :
+        if x/self.piano_label.size().width() > 0.80 and x/self.piano_label.size().width() < 0.85 and y/self.piano_label.size().width() > 0.30 and y/self.piano_label.size().width() < 0.40 :
             print("White Key 14")
-        if x/self.label_6.size().width() > 0.18 and x/self.label_6.size().width() < 0.22 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.18 and x/self.piano_label.size().width() < 0.22 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 1")
-        if x/self.label_6.size().width() > 0.23 and x/self.label_6.size().width() < 0.27 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.23 and x/self.piano_label.size().width() < 0.27 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 2")
-        if x/self.label_6.size().width() > 0.34 and x/self.label_6.size().width() < 0.37 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.34 and x/self.piano_label.size().width() < 0.37 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 3")
-        if x/self.label_6.size().width() > 0.39 and x/self.label_6.size().width() < 0.42 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.39 and x/self.piano_label.size().width() < 0.42 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 4")
-        if x/self.label_6.size().width() > 0.44 and x/self.label_6.size().width() < 0.47 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.44 and x/self.piano_label.size().width() < 0.47 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 5")
-        if x/self.label_6.size().width() > 0.54 and x/self.label_6.size().width() < 0.57 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.54 and x/self.piano_label.size().width() < 0.57 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 6")
-        if x/self.label_6.size().width() > 0.59 and x/self.label_6.size().width() < 0.62 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.59 and x/self.piano_label.size().width() < 0.62 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 7")
-        if x/self.label_6.size().width() > 0.69 and x/self.label_6.size().width() < 0.73 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.69 and x/self.piano_label.size().width() < 0.73 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 8")
-        if x/self.label_6.size().width() > 0.74 and x/self.label_6.size().width() < 0.78 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.74 and x/self.piano_label.size().width() < 0.78 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 9")
-        if x/self.label_6.size().width() > 0.79 and x/self.label_6.size().width() < 0.83 and y/self.label_6.size().width() > 0.10 and y/self.label_6.size().width() < 0.29 :
+        if x/self.piano_label.size().width() > 0.79 and x/self.piano_label.size().width() < 0.83 and y/self.piano_label.size().width() > 0.10 and y/self.piano_label.size().width() < 0.29 :
             print("Black Key 10")
        
 
